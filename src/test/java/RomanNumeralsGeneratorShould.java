45dc680bc6fcaa8f3d659f53bdbf99401f927e3d
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-25 18:46:45 +0200

    Added failing test for 14

--- src/test/java/RomanNumeralsGeneratorShould.java
+++ src/test/java/RomanNumeralsGeneratorShould.java
@@ -20,7 +20,8 @@ public class RomanNumeralsGeneratorShould {
             "10, X",
             "11, XI",
             "12, XII",
-            "13, XIII"
+            "13, XIII",
+            "14, XIV"
     })
     void convert_number_into_roman(int numberToConvert, String roman){
         RomanNumerals romanNumerals = new RomanNumerals();
