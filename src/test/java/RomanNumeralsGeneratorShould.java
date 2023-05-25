6a6d66b190c4f4bf6f31aab1b21e4e0713019001
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-25 18:13:57 +0200

    Refactor to parameterized test

--- src/test/java/RomanNumeralsGeneratorShould.java
+++ src/test/java/RomanNumeralsGeneratorShould.java
@@ -1,37 +1,22 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsGeneratorShould {
-    @Test
-    void convert_one_into_I(){
-        int numberToConvert = 1;
+    
+    @ParameterizedTest
+    @CsvSource({
+            "1, I",
+            "2, II",
+            "3, III",
+    })
+    void convert_number_into_roman(int numberToConvert, String roman){
         RomanNumerals romanNumerals = new RomanNumerals();
 
         String numberToRoman = romanNumerals.convert(numberToConvert);
 
-        String roman = "I";
-        assertEquals(roman,numberToRoman);
-    }
-
-    @Test
-    void convert_two_into_II(){
-        int numberToConvert = 2;
-        RomanNumerals romanNumerals = new RomanNumerals();
-
-        String numberToRoman = romanNumerals.convert(numberToConvert);
-
-        String roman = "II";
-        assertEquals(roman,numberToRoman);
-    }
-    @Test
-    void convert_three_into_III(){
-        int numberToConvert = 3;
-        RomanNumerals romanNumerals = new RomanNumerals();
-
-        String numberToRoman = romanNumerals.convert(numberToConvert);
-
-        String roman = "III";
-        assertEquals(roman,numberToRoman);
+        assertEquals(roman, numberToRoman);
     }
 }
