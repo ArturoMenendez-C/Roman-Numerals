f789e1ea25cae8bb0d3c3535193a6e45bc8f02e8
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-25 21:06:30 +0200

    failing test for V

--- src/test/java/RomanNumeralsGeneratorShould.java
+++ src/test/java/RomanNumeralsGeneratorShould.java
@@ -11,6 +11,7 @@ public class RomanNumeralsGeneratorShould {
             "1, I",
             "2, II",
             "3, III",
+            "5, V",
     })
     void convert_number_into_roman(int numberToConvert, String roman){
         RomanNumerals romanNumerals = new RomanNumerals();
