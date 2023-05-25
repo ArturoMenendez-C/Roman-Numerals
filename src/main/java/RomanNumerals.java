df40ed7cafe8b274ada97e0c80fac1d1e2ab4651
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-25 21:06:13 +0200

    refactor code semantic and structure

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,14 +1,5 @@
 public class RomanNumerals {
     public String convert(int numberToConvert) {
-        if(numberToConvert == 1) {
-            return "I";
-        }
-        if(numberToConvert == 2) {
-            return "II";
-        }
-        if(numberToConvert == 3) {
-            return "III";
-        }
-        return null;
+        return "I".repeat(numberToConvert);
     }
 }
