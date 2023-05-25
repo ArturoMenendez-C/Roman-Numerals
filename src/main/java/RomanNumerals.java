98ea5a0107ac5c0b6bf4add844d7155011748447
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-25 21:07:04 +0200

    failing test for V

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,5 +1,8 @@
 public class RomanNumerals {
     public String convert(int numberToConvert) {
+        if(numberToConvert == 5){
+            return "V";
+        }
         return "I".repeat(numberToConvert);
     }
 }
