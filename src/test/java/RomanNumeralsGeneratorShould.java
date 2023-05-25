da27f3393d39f9365794e019bdcbeec10e3c051f
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-25 18:06:33 +0200

    Added failing test for 2

--- src/test/java/RomanNumeralsGeneratorShould.java
+++ src/test/java/RomanNumeralsGeneratorShould.java
@@ -13,4 +13,15 @@ public class RomanNumeralsGeneratorShould {
         String roman = "I";
         assertEquals(roman,numberToRoman);
     }
+
+    @Test
+    void convert_two_into_II(){
+        int numberToConvert = 2;
+        RomanNumerals romanNumerals = new RomanNumerals();
+
+        String numberToRoman = romanNumerals.convert(numberToConvert);
+
+        String roman = "II";
+        assertEquals(roman,numberToRoman);
+    }
 }
