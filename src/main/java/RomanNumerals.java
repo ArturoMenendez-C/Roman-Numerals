321435af1f66662f787efc0d93eaeab52de02f96
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-25 22:09:45 +0200

    refactor to improve structure

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -2,17 +2,42 @@ public class RomanNumerals {
     public String convert(int numberToConvert) {
         String roman = "";
 
-        if(numberToConvert >= 10){
-            roman += "X";
-            numberToConvert -= 10;
+        for (NumeralToRoman decimalToRoman : NumeralToRoman.values()) {
+            if(numberToConvert >= decimalToRoman.decimal){
+                roman += decimalToRoman.roman;
+                numberToConvert -= decimalToRoman.decimal;
+            }
         }
-        if (numberToConvert >= 5) {
-            roman += "V";
-            numberToConvert -= 5;
-        }
-        if (numberToConvert <= 3) {
-            roman += "I".repeat(numberToConvert);
-        }
+
+//        if (numberToConvert >= NumeralToRoman.TEN.decimal) {
+//            roman += NumeralToRoman.TEN.roman;
+//            numberToConvert -= NumeralToRoman.TEN.decimal;
+//        }
+//        if (numberToConvert >= NumeralToRoman.FIVE.decimal) {
+//            roman += NumeralToRoman.FIVE.roman;
+//            numberToConvert -= NumeralToRoman.FIVE.decimal;
+//        }
+//
+//        while (numberToConvert >= NumeralToRoman.ONE.decimal) {
+//            roman += NumeralToRoman.ONE.roman;
+//            numberToConvert -= NumeralToRoman.ONE.decimal;
+//        }
+
         return roman;
+}
+
+enum NumeralToRoman {
+    TEN(10, "X"),
+    FIVE(5, "V"),
+    ONE(1, "I");
+
+    private final int decimal;
+    private final String roman;
+
+    NumeralToRoman(int decimal, String roman) {
+        this.decimal = decimal;
+
+        this.roman = roman;
     }
+}
 }
