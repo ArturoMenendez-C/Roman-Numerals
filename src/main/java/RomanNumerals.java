1487bc2d80a7891cdb0dc90bbd9abf38593ea5e3
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-25 19:29:27 +0200

    Pass failing test for 20 and extracted method for repeating I

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,27 +1,51 @@
+import java.util.Map;
+
 public class RomanNumerals {
-    public String convert(int numberToConvert) {
-        String roman = "I".repeat(numberToConvert);
-        if(numberToConvert == 4){
+    private Map<Integer, String> uniques = Map.of(
+            10, "X",
+            5, "V",
+            1, "I"
+    );
+
+    private Map<Integer, String> repeatables = Map.of(
+            1, "I",
+            10, "X"
+    );
+
+
+    public String convert(int decimal) {
+        String roman = "";
+        if(decimal >= 0) {
+            roman = getRepeatedOf("I", decimal, 0);
+        }
+        if (decimal == 4) {
             roman = "IV";
         }
-        if(numberToConvert >= 5){
-            roman = "V" + "I".repeat(numberToConvert-5);
+        if (decimal >= 5) {
+            roman = "V" + getRepeatedOf("I", decimal, 5);
         }
-        if(numberToConvert == 9){
+        if (decimal == 9) {
             roman = "IX";
         }
-        if(numberToConvert >= 10){
-            roman = "X" + "I".repeat(numberToConvert-10);
+        if (decimal >= 10) {
+            roman = "X" + getRepeatedOf("I", decimal, 10);
         }
-        if(numberToConvert == 14){
+        if (decimal == 14) {
             roman = "XIV";
         }
-        if(numberToConvert >= 15){
-            roman = "XV" + "I".repeat(numberToConvert-15);
+        if (decimal >= 15) {
+            roman = "XV" + getRepeatedOf("I", decimal, 15);
         }
-        if(numberToConvert == 19){
+        if (decimal == 19) {
             roman = "XIX";
         }
+        if(decimal == 20) {
+            roman = "XX";
+        }
         return roman;
+    }
+
+    private static String getRepeatedOf(String roman, int numberToConvert, int startingNumber) {
+        return roman.repeat(numberToConvert - startingNumber);
     }
 }
